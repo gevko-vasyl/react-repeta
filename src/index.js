c5f6c25828c7f2006dc93e413bcc0d151799a160
Vasyl Hevko <hevko.vasyl.me@gmail.com>
2021-03-27 23:21:19 +0200

    123

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-const a
- 
-if {
-  
+
+let person = {
+  name: 'Yoda',
+  designation: 'Jedi Master ',
+};
+
+function trainJedi(jediWarrion) {
+  if (jediWarrion.name === 'Yoda') {
+    console.log('No need! already trained');
+  }
+  console.log(`Training ${jediWarrion.name} complete`);
 }
+
+trainJedi(person);
+trainJedi({ name: 'Adeel', designation: 'padawan' });
